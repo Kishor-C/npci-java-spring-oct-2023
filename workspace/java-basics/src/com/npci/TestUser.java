c747d5f763bb2b3c2be849c3f1a8d6bfad02a719
Kishor <kishor1124@gmail.com>
2023-10-13 15:17:21 +0530

    Find By Name and case 3

--- workspace/java-basics/src/com/npci/TestUser.java
+++ workspace/java-basics/src/com/npci/TestUser.java
@@ -12,7 +12,7 @@ public class TestUser {
 		UserService service = new UserService(); // to call store() & findAll() 
 		int option = 0; // user option to store or findAll or exit
 		do {
-			System.out.println("1: Store 2: Find All 3: Exit");
+			System.out.println("1: Store 2: Find All 3: Find By Name -1: Exit");
 			option = scan.nextInt();
 			switch(option) {
 			case 1: 
@@ -39,8 +39,15 @@ public class TestUser {
 					System.out.println("________________________");
 				}
 				break;
+			case 3:
+				System.out.println("Enter name");
+				// name is already declared in the case 1
+				name = scan.next();
+				user = service.findUserByName(name);
+				System.out.println("Hello "+user.getName()+" your birthday = "+user.getBirthday());
+				break;
 			}
-		} while(option != 3);
-		
+		} while(option != -1);
+		// https://github.com/Kishor-C/npci-java-spring-oct-2023.git
 	}
 }
--- workspace/java-basics/src/com/npci/service/UserService.java
+++ workspace/java-basics/src/com/npci/service/UserService.java
@@ -18,6 +18,25 @@ public class UserService {
 	}
 	// findAll method
 	public User[] findAll() {
-		return users;
+		// copy the array users to another array that can store only initialized block
+		User[] copy = new User[index];
+		for(int i = 0; i < index; i++) {
+			copy[i] = users[i];
+		}
+		return copy;
+	}
+	/*
+	 * Create a method that finds the user by name and returns the User 
+	 * In Main method create case 3 to accept the name & find the user by name
+	 */
+	public User findUserByName(String name) {
+		User[] array = findAll();
+		for(User u : array) {
+			if(u.getName().equals(name)) {
+				return u;
+			}
+		}
+		// throw exception or return null
+		return null;
 	}
 }
